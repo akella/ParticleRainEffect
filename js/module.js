75bfd05ffb7c8f002b7d87e762518fed9caa0ab3
Yuriy Artyukh <akella@MacBook-Pro.local>
2021-03-17 18:53:58 +0200

    remove orbit

--- js/module.js
+++ js/module.js
@@ -54,7 +54,7 @@ export default class Sketch {
     // var aspect = window.innerWidth / window.innerHeight;
     // this.camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
     this.camera.position.set(0, 0, 150);
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.time = 0;
 
     this.urls = [this.url];
